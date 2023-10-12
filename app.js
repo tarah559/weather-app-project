c1b9e4ac593778ef5957e3eda5fd0fc01b3cdf52
tarah559 <eliezairenaica@gmail.com>
2023-10-11 22:07:26 -0400

    I created a new function called displayForecast

--- app.js
+++ app.js
@@ -15,6 +15,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  
+  let forecastHTML=`<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function(day) {
+
+  forecastHTML = forecastHTML + `
+        <div class="col-2">
+            <div class="weather-forecast-date">
+            ${day}
+            </div>
+            <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">
+                    12°
+                </span>
+                <span class="weather-forecast-temperature-min">
+                    18°
+                </span>
+            </div>
+        </div>     
+  `;
+   })
+
+  forecastHTML = forecastHTML + `</div`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -68,6 +96,7 @@ function showCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -78,3 +107,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 search("London");
+displayForecast();
